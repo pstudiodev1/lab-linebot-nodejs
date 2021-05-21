031a812c4e699350d592be3a552acb1769fd1a08
pstudiodev1 <pstudiodev@gmail.com>
2021-05-21 15:22:19 +0700

    First version

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.post('/webhook', (req, res) => {
+    console.log('POST ==> /webhook');
+    res.sendStatus(200);
+});
+
+app.get('/webhook', (req, res) => {
+    console.log('GET ==> /webhook');
+    res.sendStatus(200);
+});
+
+app.get('/', (req, res) => {
+    console.log('GET ==> /');
+    res.sendStatus(200);
+});
+
+app.listen(port)
