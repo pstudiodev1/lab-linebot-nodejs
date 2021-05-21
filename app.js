e83e9734d6ac3056aa954d0c915ad74da4d7a0b3
pstudiodev1 <pstudiodev@gmail.com>
2021-05-21 16:25:47 +0700

    Reply echo msg

--- app.js
+++ app.js
@@ -10,17 +10,31 @@ app.use(bodyParser.json());
 function reply(reply_token) {
     let headers = {
         'Content-Type': 'application/json',
-        'Authorization': 'Bearer {xxxxxxx}'
+        'Authorization': 'Bearer {o34/mccX3IEkCbi+HtubJTIAEHOR7iT7uptpbU/huWMWS86rpydlbnm9nLmxUBoJwebLI1xW3x5xJwbCw42bqbaS5hTbtg2fl24I+DBkK3KJZtcHOY3jH/Rj8xyRhpfEpJcV6lmmaC9+nFEE4X6o7gdB04t89/1O/w1cDnyilFU=}'
     }
+    // let body = JSON.stringify({
+    //     replyToken: reply_token,
+    //     messages: [{
+    //         type: 'text',
+    //         text: 'Hello'
+    //     },
+    //     {
+    //         type: 'text',
+    //         text: 'How are you?'
+    //     }]
+    // })
+    // request.post({
+    //     url: 'https://api.line.me/v2/bot/message/reply',
+    //     headers: headers,
+    //     body: body
+    // }, (err, res, body) => {
+    //     console.log('status = ' + res.statusCode);
+    // });
     let body = JSON.stringify({
         replyToken: reply_token,
         messages: [{
             type: 'text',
-            text: 'Hello'
-        },
-        {
-            type: 'text',
-            text: 'How are you?'
+            text: msg
         }]
     })
     request.post({
@@ -34,8 +48,12 @@ function reply(reply_token) {
 
 app.post('/webhook', (req, res) => {
     console.log('POST ==> /webhook');
+    // console.log(req.body);
+    // let reply_token = req.body.events[0].replyToken;
+    // reply(reply_token);
     let reply_token = req.body.events[0].replyToken
-    reply(reply_token)
+    let msg = req.body.events[0].message.text
+    reply(reply_token, msg)
     res.sendStatus(200);
 });
 
