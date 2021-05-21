47bf31c9f0af8af0d537e2182dc78d77406691f8
pstudiodev1 <pstudiodev@gmail.com>
2021-05-21 15:55:23 +0700

    Add body parser

--- app.js
+++ app.js
@@ -1,9 +1,41 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
 const app = express();
 const port = process.env.PORT || 4000;
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+function reply(reply_token) {
+    let headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer {xxxxxxx}'
+    }
+    let body = JSON.stringify({
+        replyToken: reply_token,
+        messages: [{
+            type: 'text',
+            text: 'Hello'
+        },
+        {
+            type: 'text',
+            text: 'How are you?'
+        }]
+    })
+    request.post({
+        url: 'https://api.line.me/v2/bot/message/reply',
+        headers: headers,
+        body: body
+    }, (err, res, body) => {
+        console.log('status = ' + res.statusCode);
+    });
+}
+
 app.post('/webhook', (req, res) => {
     console.log('POST ==> /webhook');
+    let reply_token = req.body.events[0].replyToken
+    reply(reply_token)
     res.sendStatus(200);
 });
 
